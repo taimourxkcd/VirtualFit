5ab1281d836602a7c55f633c18f4e82865e73eea
taimourz <taimour.rkt@gmail.com>
2023-06-04 14:14:50 +0500

    fixed images imports

--- fyp_project_website/app.py
+++ fyp_project_website/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
@@ -6,5 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/run-main')
+def run_main():
+    subprocess.Popen(['python', 'pose_tracking/main.py'])
+    return 'Running main.py'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- fyp_project_website/static/js/cam.js
+++ fyp_project_website/static/js/cam.js
@@ -1,39 +1,53 @@
-const openWebcamButton = document.querySelector("#openWebcamButton");
-
-openWebcamButton.addEventListener("click", () => {
-  const constraints = {
-    video: true,
-    // audio: true,
-  };
-
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        const newWindow = window.open(
-          "",
-          "Webcam Video",
-          "width=640,height=480"
-        );
-        newWindow.document.write(
-          '<video id="myVideo" controls autoplay muted></video>'
-        );
-        const video = newWindow.document.querySelector("#myVideo");
-        if (stream) {
-          video.srcObject = stream;
-          video.play();
-        video.setAttribute("autoplay", true);
-        video.onloadedmetadata = function(ev){
-            video.play();
-        }
-        } else {
-          console.error("Stream is not available");
-        }
-
-        
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+$(document).ready(function () {
+  $("#openWebcamButton").click(function () {
+    $.ajax({
+      url: "/run-main",
+      type: "GET",
+      success: function (response) {
+        console.log("main.py executed successfully");
+      },
+      error: function (error) {
+        console.log("An error occurred while executing main.py");
+      },
+    });
+  });
 });
+
+// const openWebcamButton = document.querySelector("#openWebcamButton");
+
+// openWebcamButton.addEventListener("click", () => {
+//   const constraints = {
+//     video: true,
+//     // audio: true,
+//   };
+
+//   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//     navigator.mediaDevices
+//       .getUserMedia(constraints)
+//       .then((stream) => {
+//         const newWindow = window.open(
+//           "",
+//           "Webcam Video",
+//           "width=640,height=480"
+//         );
+//         newWindow.document.write(
+//           '<video id="myVideo" controls autoplay muted></video>'
+//         );
+//         const video = newWindow.document.querySelector("#myVideo");
+//         if (stream) {
+//           video.srcObject = stream;
+//           video.play();
+//         video.setAttribute("autoplay", true);
+//         video.onloadedmetadata = function(ev){
+//             video.play();
+//         }
+//         } else {
+//           console.error("Stream is not available");
+//         }
+
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   }
+// });
